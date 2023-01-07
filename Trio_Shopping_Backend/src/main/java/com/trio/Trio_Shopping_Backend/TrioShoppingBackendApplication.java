64f57e018723477262bf415d8973087b4337f470
Bajrangi Lal <bajrangilalb612@gmail.com>
2023-01-07 17:05:35 +0530

    main Code

---
+++ Trio_Shopping_Backend/src/main/java/com/trio/Trio_Shopping_Backend/TrioShoppingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trio.Trio_Shopping_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrioShoppingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrioShoppingBackendApplication.class, args);
+	}
+
+}
