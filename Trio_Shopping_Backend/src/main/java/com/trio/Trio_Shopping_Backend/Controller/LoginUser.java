5baa2697241434e99568b2da1e0c65b9b2c84b2f
Ankit kumar khatri <ankitkhatri988@gmail.com>
2023-01-08 15:12:07 +0530

    Login class changes

--- Trio_Shopping_Backend/src/main/java/com/trio/Trio_Shopping_Backend/Controller/LoginUser.java
+++ Trio_Shopping_Backend/src/main/java/com/trio/Trio_Shopping_Backend/Controller/LoginUser.java
@@ -1,4 +1,9 @@
 package com.trio.Trio_Shopping_Backend.Controller;
-
+@Controller
 public class LoginUser {
+    @ReqquestMapping('/Ankit')
+    @Responsebody
+    public String Demo(){
+        return "Ankit";
+    }
 }
